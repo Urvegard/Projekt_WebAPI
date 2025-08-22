5c9df1d7138e6d2f421ad98e48aa2029316a260d
Urvegard <rikard.urvegard@gmail.com>
2025-08-22 14:30:17 +0200

    Add project files.

---
+++ Data/TravelContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projekt_WebAPI.Models;
+
+namespace Projekt_WebAPI.Data
+{
+    public class TravelContext:DbContext
+    {
+        public TravelContext()
+        {
+            
+        }
+
+        public TravelContext(DbContextOptions<TravelContext> options) : base(options)
+        {
+            
+        }
+
+        public virtual DbSet<City> Cities { get; set; }
+
+    }
+}
---
+++ Models/City.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projekt_WebAPI.Models
+{
+    public class City
+    {
+        [Key] //Primary Key
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
